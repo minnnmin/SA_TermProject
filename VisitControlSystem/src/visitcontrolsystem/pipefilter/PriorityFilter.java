846dc732e1018957b8da21685d960204702a7976
kan5016@naver.com <kan5016@naver.com>
2021-12-15 22:03:18 +0900

    [update] FilterV apply priority(by Vaccination and Age)

--- VisitControlSystem/src/visitcontrolsystem/pipefilter/PriorityFilter.java
+++ VisitControlSystem/src/visitcontrolsystem/pipefilter/PriorityFilter.java
@@ -33,17 +33,43 @@ class PriorityV implements Comparator<Object> {
 	 
     @Override
     public int compare(Object o1, Object o2) {
+		// visitor 은 백신접종여부와 나이에 따라 우선순위 전송
     	Visitor v1 = (Visitor) o1;
     	Visitor v2 = (Visitor) o2;
-        return v1.getVid().compareTo(v2.getVid());
+
+		if(v1.getVaccination().compareTo(v2.getVaccination()) != 0){
+			// 백신접종여부가 다를 경우, 작은 값이 더 앞으로 오도록 정렬
+			return v1.getVaccination().compareTo(v2.getVaccination()); 
+		} else {
+			// 백신접종여부가 같을 경우, 나이에 따라 우선순위 정렬
+			int v1AgePriority = 0;
+			if(v1.getVage() >= 60){
+				v1AgePriority = 1; // 60세 이상 1순위
+			} else if (v1.getVage() <= 15){
+				v1AgePriority = 2; // 15세 이하 2순위
+			} else {
+				v1AgePriority = 3; // 나머지 3순위
+			}
+
+			int v2AgePriority = 0;
+			if(v2.getVage() >= 60){
+				v2AgePriority = 1; // 60세 이상 1순위
+			} else if (v2.getVage() <= 15){
+				v2AgePriority = 2; // 15세 이하 2순위
+			} else {
+				v2AgePriority = 3; // 나머지 3순위
+			}
+
+			return Integer.compare(v1AgePriority, v2AgePriority);
+		}
     }
- 
 }
 
 class PriorityF implements Comparator<Object> {
-	 
+
     @Override
     public int compare(Object o1, Object o2) {
+    	// facilty 는 id 값 우선순위로 전송
     	Facility v1 = (Facility) o1;
     	Facility v2 = (Facility) o2;
         return v1.getFid().compareTo(v2.getFid());
