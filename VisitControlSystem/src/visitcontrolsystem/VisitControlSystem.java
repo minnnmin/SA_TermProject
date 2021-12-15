732f8d94bf126bef5fdbcc5c1672d92e5e8082e5
minnmin <rhkr_alswjd@naver.com>
2021-12-15 23:53:52 +0900

    - UI: 가데이터 추가 및 테스트 케이스 세팅 - 시설 메시지 수정

--- VisitControlSystem/src/visitcontrolsystem/VisitControlSystem.java
+++ VisitControlSystem/src/visitcontrolsystem/VisitControlSystem.java
@@ -19,15 +19,7 @@ import java.util.List;
 
 public class VisitControlSystem {
     public static void main(String[] args) {
-//        시설, 고객 데이터
-        Facility f1 = new Facility("cafe1", "서울", "1234");
-        Facility f2 = new Facility("cafe2", "제주", "5678");
-        Facility f3 = new Facility("cafe3", "강릉", "3456");
-        Visitor v1 = new Visitor("minjeong", 24, "제주", "010-1111-1111", Vaccination.UNVACCINATED);
-        Visitor v2 = new Visitor("kyewon", 25, "서울", "010-2222-2222", Vaccination.FIRST_VACCINATION_COMPLETED);
-        Visitor v3 = new Visitor("changseok", 26, "부산", "010-3333-3333", Vaccination.SECOND_VACCINATION_COMPLETED);
-        Visitor v4 = new Visitor("honggildong", 27, "경기", "010-4444-4444", Vaccination.SECOND_VACCINATION_COMPLETED);
-
+        
         ManagementServer managementServer = new ManagementServer();
         ServerConnector serverConnector = new ServerConnector();
         serverConnector.connectServer(managementServer);
@@ -40,41 +32,89 @@ public class VisitControlSystem {
         FHandler fHandler = new FHandler(visitlistControlModule);
         CHandler cHandler = new CHandler(visitlistControlModule,encryptModule);
         HHandler hHandler = new HHandler(searchProcessModule,encryptModule);
+        
+        // 시설, 고객 데이터
+        Facility f1 = new Facility("cafe1", "서울", "010-1234-1234");
+        Facility f2 = new Facility("cafe2", "경기", "010-2345-2345");
+        Facility f3 = new Facility("cafe3", "대전", "010-3456-3456");
+        Facility f4 = new Facility("cafe4", "부산", "010-4567-4567");
+        Facility f5 = new Facility("cafe5", "제주", "010-5678-5678");
+        Visitor v1 = new Visitor("곽민정", 12, "제주", "010-1111-1111", Vaccination.SECOND_VACCINATION_COMPLETED);
+        Visitor v2 = new Visitor("전계원", 14, "서울", "010-2222-2222", Vaccination.UNVACCINATED);
+        Visitor v3 = new Visitor("우창석", 18, "부산", "010-3333-3333", Vaccination.SECOND_VACCINATION_COMPLETED);
+        Visitor v4 = new Visitor("이민호", 25, "대전", "010-4444-4444", Vaccination.SECOND_VACCINATION_COMPLETED);
+        Visitor v5 = new Visitor("김준우", 35, "경기", "010-5555-5555", Vaccination.FIRST_VACCINATION_COMPLETED);
+        Visitor v6 = new Visitor("최지원", 40, "서울", "010-6666-6666", Vaccination.SECOND_VACCINATION_COMPLETED);
+        Visitor v7 = new Visitor("강승완", 45, "부산", "010-7777-7777", Vaccination.FIRST_VACCINATION_COMPLETED);
+        Visitor v8 = new Visitor("정제윈", 52, "경기", "010-8888-8888", Vaccination.SECOND_VACCINATION_COMPLETED);
+        Visitor v9 = new Visitor("이재원", 60, "서울", "010-9999-9999", Vaccination.UNVACCINATED);
+        Visitor v10 = new Visitor("최지혜", 72, "부산", "010-0000-0000", Vaccination.SECOND_VACCINATION_COMPLETED);
 
+        // 명부개설 & 방문기록 추가
+        // f1에 v1 ~ v10 방문
         fHandler.createVisitlist(f1);
-        cHandler.addVisitorInfo(f1, v1, "20211001");
-        cHandler.addVisitorInfo(f1, v2, "20211002");
-        cHandler.addVisitorInfo(f1, v3, "20211003");
+        cHandler.addVisitorInfo(f1, v1, "20211201");
+        cHandler.addVisitorInfo(f1, v2, "20211201");
+        cHandler.addVisitorInfo(f1, v3, "20211202");
+        cHandler.addVisitorInfo(f1, v4, "20211202");
+        cHandler.addVisitorInfo(f1, v5, "20211205");
+        cHandler.addVisitorInfo(f1, v6, "20211205");
+        cHandler.addVisitorInfo(f1, v7, "20211206");
+        cHandler.addVisitorInfo(f1, v8, "20211206");
+        cHandler.addVisitorInfo(f1, v9, "20211206");
+        cHandler.addVisitorInfo(f1, v10, "20211207");
 
+        // f2에 v1 ~ v5 방문
         fHandler.createVisitlist(f2);
-        cHandler.addVisitorInfo(f2, v1, "20211101");
-        cHandler.addVisitorInfo(f2, v2, "20211102");
-        cHandler.addVisitorInfo(f2, v3, "20211103");
+        cHandler.addVisitorInfo(f2, v1, "20211211");
+        cHandler.addVisitorInfo(f2, v2, "20211211");
+        cHandler.addVisitorInfo(f2, v3, "20211212");
+        cHandler.addVisitorInfo(f2, v4, "20211212");
+        cHandler.addVisitorInfo(f2, v5, "20211214");
 
+        // f3에 v6 ~ v10 방문
         fHandler.createVisitlist(f3);
-        cHandler.addVisitorInfo(f3, v1, "20211201");
-        cHandler.addVisitorInfo(f3, v2, "20211202");
-        cHandler.addVisitorInfo(f3, v3, "20211203");
+        cHandler.addVisitorInfo(f3, v6, "20211215");
+        cHandler.addVisitorInfo(f3, v7, "20211216");
+        cHandler.addVisitorInfo(f3, v8, "20211216");
+        cHandler.addVisitorInfo(f3, v9, "20211216");
+        cHandler.addVisitorInfo(f3, v10, "20211217");
 
-//        명부 개설, 방문기록 추가 테스트
-        cHandler.printVisitorsOf(f1);
-        cHandler.printVisitorsOf(f2);
-        cHandler.printVisitorsOf(f3);
+        // f4에 v1, 3, 5, 6, 7, 9 방문
+        fHandler.createVisitlist(f4);
+        cHandler.addVisitorInfo(f4, v1, "20211202");
+        cHandler.addVisitorInfo(f4, v3, "20211208");
+        cHandler.addVisitorInfo(f4, v5, "20211209");
+        cHandler.addVisitorInfo(f4, v6, "20211215");
+        cHandler.addVisitorInfo(f4, v7, "20211218");
+        cHandler.addVisitorInfo(f4, v9, "20211220");
 
-//        searchFacilityList 테스트
-        ArrayList<Facility> facilityList = hHandler.searchFacilityList(v3, "20210901", "20211130");
+        // f5에 v2, 3, 4, 6, 8, 10 방문
+        fHandler.createVisitlist(f4);
+        cHandler.addVisitorInfo(f4, v2, "20211201");
+        cHandler.addVisitorInfo(f4, v3, "20211205");
+        cHandler.addVisitorInfo(f4, v4, "20211207");
+        cHandler.addVisitorInfo(f4, v6, "20211215");
+        cHandler.addVisitorInfo(f4, v8, "20211223");
+        cHandler.addVisitorInfo(f4, v10, "20211231");
+
+
+        // 특정 기간 내 확진자(v3)가 다녀간 시설 리스트
+        System.out.println("[특정 기간 내 확진자가 다녀간 시설 리스트]");
+        ArrayList<Facility> facilityList = hHandler.searchFacilityList(v3, "20211201", "20211212");
         if (facilityList.isEmpty()) {
-            System.out.println("해당하는 시설 없음");
+            System.out.println("해당 기간 동안 확진자가 다녀간 시설이 없습니다.");
         } else {
             for (Facility facility : facilityList) {
                 System.out.println(facility.getFname());
             }
         }
 
-//        searchVisitorList 테스트
-        ArrayList<String> visitorList = hHandler.searchVisitorList(f1, "20211001", "20211002");
+        // 특정 기간 내 해당 시설(f1)에 다녀간 방문자 리스트
+        System.out.println("\n[특정 기간 내 해당 시설에 다녀간 방문자 리스트]");
+        ArrayList<String> visitorList = hHandler.searchVisitorList(f1, "20211201", "20211208");
         if (visitorList.isEmpty()) {
-            System.out.println("해당하는 방문객 없음");
+            System.out.println("해당 기간 동안 시설에 다녀간 방문객이 없습니다.");
         } else {
             for (String visitorEnc : visitorList) {
                 System.out.println(visitorEnc);
@@ -83,16 +123,14 @@ public class VisitControlSystem {
 
         // visitcontrolsystem.module.NotificationControlModule 테스트
         NotificationControlModule nofi = new NotificationControlModule();
-        List<Facility> fs = new ArrayList();
-        fs.add(f1);
-        fs.add(f3);
-        fs.add(f2);
 
         List<String> vs = new ArrayList();
         for (String visitorEnc: visitorList){
             vs.add(visitorEnc);
         }
-        HMessage hm = new HMessage(fs, vs);
+
+        System.out.println("\n[우선순위 별 알림 전송]");
+        HMessage hm = new HMessage(facilityList, vs);
         nofi.sendMessage(hm);
     }
 }
--- VisitControlSystem/src/visitcontrolsystem/model/PackMessage.java
+++ VisitControlSystem/src/visitcontrolsystem/model/PackMessage.java
@@ -25,7 +25,7 @@ public class PackMessage {
 			StringBuilder sb = new StringBuilder();
 			sb.append("[코로나 확진판별 검사 안내]\n");
 			sb.append("\"" + visitor.getFname() + "\"" + "관리자님 역학조사 결과 코로나 확진자가 해당 시설에 방문한 것으로 확인되었습니다.\n");
-			sb.append("2주간 가게 임시휴업 조치 및 방역조치 부탁드리며, 관리자님 또한 인근 선별진료소를 통해 확진여부를 검사해주시길 바랍니다.\n");
+			sb.append("방역이 완료될 때까지 임시휴업 조치 부탁드리며, 관리자님 또한 인근 선별진료소를 통해 확진여부를 검사해주시길 바랍니다.\n");
 			System.out.println(sb.toString());
 		}
 		else {
